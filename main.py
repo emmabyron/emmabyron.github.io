15639a4fe60ffa305425cb1cd53e179f9dd3b66b
Emma Byron <emmabyron73@gmail.com>
2022-08-27 09:55:14 -0400

    did a python :)

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 tournaments = open("tournaments.txt", "r")
-db = open("db.txt", "r")
+db = open("db.txt", "w")
+
+array = [i.split("; ") for i in tournaments.read().split("\n")]
+
+output = ""
+
+for i in array:
+    output += "<tr>"
+
+    for j in i:
+        output += "<td>"
+        output += j
+        output += "</td>"
+    
+    output += "<tr>"
+
+db.write("var db = \"" + output + "\"")
 
 tournaments.close()
 db.close()
