e0b3d9f93d2a85a374ac56cfb0af39813c13a6f1
Emma Byron <emmabyron73@gmail.com>
2022-08-27 10:22:35 -0400

    gfdi github pages just let me have this feature aa

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById("tournaments").innerHTML = db;
+db = db.split("\n").map(function semicolonSplit(string) {return string.split("; ");});
+
+var output = "";
+
+for (let i of db) {
+    output += "<tr>";
+
+    for (let j of i) {
+        output += "<td>";
+        output += j;
+        output += "</td>";
+    }
+
+    output += "</tr>";
+}
+
+document.getElementById("tournaments").innerHTML = output;
